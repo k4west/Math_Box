d58edf08ebab666cbd15bc6da3ddde2fe560a4da
joseph <kwestmin@gmail.com>
2025-02-10 22:28:44 +0900

    Create calculator.py and License

---
+++ calculator.py
@@ -0,0 +1,6 @@
+
+ded[B[B[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C:[C[C[C[C[D[D[D[D[B[D[B[A[D[D[A[D[A[A[D[D[B[D[D[D[D[D[D[D[D[Ddef add(a, b):[C[D
+
+[A[B[[D[A[C[C[C[C[C[C[C    return a+b
+def subtract(a, b):
+    return a-b
